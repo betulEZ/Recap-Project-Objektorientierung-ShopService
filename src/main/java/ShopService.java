819d6814463e53eb07eb92d0f464cc3d3316e653
BetulEzgu <33276357+betulEZ@users.noreply.github.com>
2024-02-12 20:34:21 +0100

    add throws

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -9,13 +9,12 @@ public class ShopService {
     private OrderRepo orderRepo = new OrderMapRepo();
 
 
-    public Order addOrder(List<String> productIds) {
+    public Order addOrder(List<String> productIds) throws NullPointerException{
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
            Optional<Product> productToOrder = productRepo.getProductById(productId);
             if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
+                throw new NullPointerException ("Product does not exist.");
             }
             products.add(productToOrder.get());
         }
